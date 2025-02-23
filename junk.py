dce2ac59b52b3dd37e870c3d20217015bd1233d5
Prakash Kumar Pawar <prakashkumarpawar@gmail.com>
2025-02-23 23:41:50 +0530

    code

--- junk.py
+++ junk.py
@@ -51,34 +51,107 @@
 # insertion(l)
 # print(f"After Sorting ::{l}")
 
-def mergesort(a,b):
-    res = []
-    m = len(a)
-    n = len(b)
-    i = 0
-    j = 0
+# def mergesort(a,b):
+#     res = []
+#     m = len(a)
+#     n = len(b)
+#     i = 0
+#     j = 0
 
-    while i < m and j < n:
-        if a[i] < b[j]:
-            res.append(a[i])
-            i = i+1
+#     while i < m and j < n:
+#         if a[i] < b[j]:
+#             res.append(a[i])
+#             i = i+1
+
+#         else:
+#             res.append(b[j])
+#             j = j+1
+
+#     while i < m:
+#         res.append(a[i])
+#         i = i + 1
+#     while j < n:
+#         res.append(b[j])
+#         j = j + 1
+#     return res
+
+
+# b = [2,5,7,9,11,13]
+# a = [4,6,8]
+
+# print(f"Before Sorting ::{a} {b}")
+# sor = mergesort(a,b)
+# print(f"After Sorting ::{sor}")
+
+
+# Nth Node from end of linked list
+
+# Recursive Reverse a linked list
+
+
+class Node:
+    def __init__(self, data):
+        self.key = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
 
         else:
-            res.append(b[j])
-            j = j+1
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = new_node
 
-    while i < m:
-        res.append(a[i])
-        i = i + 1
-    while j < n:
-        res.append(b[j])
-        j = j + 1
-    return res
+    def reverseList(self, curr, prev= None):
+        if curr == None:
+            return prev
+        
+        next = curr.next
+        curr.next = prev
+ 
+        #Recursively reverse the rest of the list
+        return self.reverseList(next, curr)
+        
+    def printlist(self):
+        curr = self.head
+        while curr:
+            print(curr.key, end=" ->")
+            curr = curr.next
+        print("None")
+    
+
+    def reverse(self):
+        # Start the reversal process from the head of the list
+        self.head = self.reverseList(self.head)
 
 
-b = [2,5,7,9,11,13]
-a = [4,6,8]
 
-print(f"Before Sorting ::{a} {b}")
-sor = mergesort(a,b)
-print(f"After Sorting ::{sor}")
+
+# Create a LinkedList
+ll = LinkedList()
+# Insert nodes into the linked list while maintaining the sorted order
+ll.append(10)
+ll.append(20)
+ll.append(20)
+ll.append(30)
+ll.append(30)
+ll.append(30)
+
+
+# Print the linked list to see the sorted result
+print("Sorted Linked List:")
+ll.printlist()
+ll.reverse()
+ll.printlist()
+    
+
+
+
+            
